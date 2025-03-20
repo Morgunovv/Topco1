ab815ce2be0700eaf01dee6e438ff64fa8c1bcb8
Vasiliy <9392035@gmail.com>
2025-03-20 14:42:10 +0100

    Добавлены шрифты InterDisplay и Inter

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import type { AppProps } from 'next/app';
+import { ApolloProvider } from '@apollo/client';
+import { client } from '../lib/apollo-client';
+import '../styles/fonts.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <ApolloProvider client={client}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    );
+}
+
+export default MyApp; 
