204c3c693538816321b3516255188ddd5e7c16e6
Vasiliy <9392035@gmail.com>
2025-03-20 22:21:19 +0100

    Fix production configuration and error handling

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ const nextConfig = {
     // Удаляем experimental.appDir
     images: {
         domains: ['static1.plasmic.app', 'img.plasmic.app']
-    }
+    },
+    // Добавим настройки для production
+    productionBrowserSourceMaps: true, // для лучшей отладки
+    poweredByHeader: false
 }
 
 module.exports = nextConfig 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import type { AppProps } from 'next/app';
 import { PlasmicRootProvider } from "@plasmicapp/loader-nextjs";
 import { PLASMIC } from "../plasmic-init";
+import Error from 'next/error';
 
 export default function App({ Component, pageProps }: AppProps) {
+    if (pageProps.error) {
+        return <Error
+            statusCode={pageProps.error.statusCode}
+            title={pageProps.error.message}
+        />;
+    }
+
     return (
         <PlasmicRootProvider
             loader={PLASMIC}
             prefetchedData={pageProps.plasmicData}
+            pageParams={pageProps.plasmicParams}
         >
             <Component {...pageProps} />
         </PlasmicRootProvider>
