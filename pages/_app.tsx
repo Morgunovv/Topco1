651c5fbd4345dccdf5036797f51c81c2a8b5738f
Vasiliy <9392035@gmail.com>
2025-03-20 14:49:50 +0100

    Добавлены шрифты

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,54 @@
 import type { AppProps } from 'next/app';
-import { ApolloProvider } from '@apollo/client';
-import { client } from '../lib/apollo-client';
-import '../styles/fonts.css';
+import localFont from 'next/font/local';
+import * as React from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-    return (
-        <ApolloProvider client={client}>
-            <Component {...pageProps} />
-        </ApolloProvider>
-    );
-}
+// Определяем все варианты шрифта InterDisplay
+const interDisplay = localFont({
+    src: [
+        {
+            path: '../public/fonts/InterDisplay-Thin.woff2',
+            weight: '100',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/InterDisplay-Light.woff2',
+            weight: '300',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/InterDisplay-Regular.woff2',
+            weight: '400',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/InterDisplay-SemiBold.woff2',
+            weight: '600',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/InterDisplay-Bold.woff2',
+            weight: '700',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/InterDisplay-ExtraBold.woff2',
+            weight: '800',
+            style: 'normal',
+        },
+        {
+            path: '../public/fonts/InterDisplay-Black.woff2',
+            weight: '900',
+            style: 'normal',
+        }
+    ],
+    variable: '--font-inter-display'
+});
 
-export default MyApp; 
+export default function MyApp({ Component, pageProps }: AppProps) {
+    // Добавляем CSS переменную в body
+    React.useEffect(() => {
+        document.body.classList.add(interDisplay.variable);
+    }, []);
+
+    return <Component {...pageProps} />;
+} 
