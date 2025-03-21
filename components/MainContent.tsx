a4d35a7c54570abe2a90f3438bb4615120add856
Vasiliy <9392035@gmail.com>
2025-03-21 17:52:07 +0100

    fix: add ESLint and fix TypeScript errors

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -2,6 +2,23 @@ import { useQuery } from '@apollo/client';
 import { PlasmicComponent } from '@plasmicapp/loader-nextjs';
 import { GET_MAINS } from '../lib/queries';
 
+// Определим интерфейс для item
+interface MainItem {
+    id: string | number;
+    attributes: {
+        title?: string;
+        description?: string;
+        // добавьте другие поля, которые есть в ваших данных
+    };
+}
+
+// Определим интерфейс для data
+interface MainData {
+    mains: {
+        data: MainItem[];
+    };
+}
+
 export function MainContent() {
     const { loading, error, data } = useQuery(GET_MAINS);
 
@@ -10,12 +27,13 @@ export function MainContent() {
 
     return (
         <div>
-            {data.mains.data.map(item => (
+            {data.mains.data.map((item: MainItem) => (
                 <PlasmicComponent
                     key={item.id}
                     component="ProjectCard"
                     componentProps={{
-                        text: item.attributes.Text,
+                        title: item.attributes.title,
+                        description: item.attributes.description,
                         // другие пропсы
                     }}
                 />
