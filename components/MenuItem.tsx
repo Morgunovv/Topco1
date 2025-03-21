aaa627dfc7749570d5e936ec8cc8e080a2392c19
Vasiliy <9392035@gmail.com>
2025-03-21 16:27:27 +0100

    fix: use @plasmicpkgs/antd5 instead of antd to avoid conflicts

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import { Menu } from 'antd';
-import type { MenuItemProps } from 'antd/lib/menu/MenuItem';
-// или если вы используете @plasmicpkgs/antd5
 import { Menu } from '@plasmicpkgs/antd5';
+import type { MenuItemProps } from '@plasmicpkgs/antd5/dist/Menu';
 import { PlasmicComponent } from '@plasmicapp/loader-nextjs';
 
-// Используем тип из antd для пропсов
-export interface CustomMenuItemProps extends MenuItemProps {
-    children?: React.ReactNode;
-}
-
-export const MenuItem: React.FC<CustomMenuItemProps> = ({ children, ...props }) => {
-    return <Menu.Item {...props}>{children}</Menu.Item>;
+// Используем только один импорт Menu из @plasmicpkgs/antd5
+export const MenuItem: React.FC<MenuItemProps> = (props) => {
+    return <Menu.Item {...props} />;
 };
 
 export function MenuItemComponent(props) {
     return <PlasmicComponent component="MenuItem" {...props} />;
 }
-
-export const MenuItem = Menu.Item;
 
 export type { MenuItemProps }; 
 } 
--- components/MenuSection.tsx
+++ components/MenuSection.tsx
@@ -1,12 +1,13 @@
-import { Menu } from 'antd';
-import type { MenuProps } from 'antd';
+import React from 'react';
+import { Menu } from '@plasmicpkgs/antd5';
+import type { MenuProps } from '@plasmicpkgs/antd5/dist/Menu';
 
 // Используем тип из antd для пропсов
 export interface MenuSectionProps extends MenuProps {
     children?: React.ReactNode;
 }
 
-export const MenuSection: React.FC<MenuSectionProps> = ({ children, ...props }) => {
+export const MenuSection: React.FC<MenuProps> = ({ children, ...props }) => {
     return (
         <Menu mode="horizontal" {...props}>
             {children}
