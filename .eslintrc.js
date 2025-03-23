237e4956782ed799d09731d111154e63df930503
Vasiliy <9392035@gmail.com>
2025-03-23 16:31:59 +0100

    fix: update ESLint configuration to ignore Plasmic files

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/no-explicit-any': 'warn',
         '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-empty-interface': 'off'
     },
     ignorePatterns: ['**/plasmic/**/*'],
     settings: {
