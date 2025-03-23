6eb603dc6332523bf1ea8310dbdc4bb30011e786
Vasiliy <9392035@gmail.com>
2025-03-23 16:56:51 +0100

    fix: update ProjectsList to use DataProvider from plasmic-strapi

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { PlasmicComponent } from "@plasmicapp/loader-nextjs";
-import { StrapiProvider, StrapiField } from "@plasmicpkgs/plasmic-strapi";
+import { DataProvider } from "@plasmicpkgs/plasmic-strapi";
 
 interface Project {
     id: string | number;
@@ -13,25 +13,27 @@ interface Project {
 
 export function ProjectsList() {
     return (
-        <StrapiProvider>
-            <StrapiField path="/api/projects">
-                {(data: { projects?: { data: Project[] } }) => (
-                    <div>
-                        {data?.projects?.data?.map((project: Project) => (
-                            <PlasmicComponent
-                                key={project.id}
-                                component="ProjectCard"
-                                componentProps={{
-                                    title: project.attributes.title,
-                                    description: project.attributes.description,
-                                    // другие пропсы
-                                }}
-                            />
-                        ))}
-                    </div>
-                )}
-            </StrapiField>
-        </StrapiProvider>
+        <DataProvider
+            name="projects"
+            apiUrl={process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'}
+            path="/api/projects"
+        >
+            {(data: { data?: Project[] }) => (
+                <div>
+                    {data?.data?.map((project: Project) => (
+                        <PlasmicComponent
+                            key={project.id}
+                            component="ProjectCard"
+                            componentProps={{
+                                title: project.attributes.title,
+                                description: project.attributes.description,
+                                // другие пропсы
+                            }}
+                        />
+                    ))}
+                </div>
+            )}
+        </DataProvider>
     );
 }
 
